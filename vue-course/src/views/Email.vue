c38444e2d49a82818b12f1d53f8c8eb47e118ea9
TieriaDM <tieria0512@163.com>
2020-05-02 22:53:42 +0800

    不同组件间传值

--- vue-course/src/views/Email.vue
+++ vue-course/src/views/Email.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
-    I am Email.
+  <div class="email">
+    <button @click="handleClick">按我</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    handleClick () {
+      // $emit 是发射当前实例上的指定事件
+      this.$bus.$emit('on-click', 'hello')
+    }
+  },
+  mounted () {
+    console.log(this.$bus)
+  }
 }
+/**
+ * 不同组件间的传值bus
+ * 1.传值的逻辑是先创建一个vue实例bus，并注册到全局
+ * 2.在合适时机，组件a使用bus发射事件b和参数
+ * 3.组件c监听bus的事件b，并获取参数，完成传值
+**/
 </script>
+<style scoped>
+.email {
+  border: 1px solid green;
+}
+</style>
--- vue-course/src/views/Tel.vue
+++ vue-course/src/views/Tel.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    I am Tel.
+  <div class="tel">
+    <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      message: ''
+    }
+  },
+  mounted () {
+    // $on 给当前实例绑定指定监听
+    this.$bus.$on('on-click', mes => {
+      this.message = mes
+    })
+  }
 }
 </script>
+<style scoped>
+.tel {
+  border: 1px solid red;
+}
+</style>
